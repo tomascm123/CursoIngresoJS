b908b18585ad2a38a6d4822b5e9d29f76853d8b0
tomascm123 <40763524+tomascm123@users.noreply.github.com>
2018-07-05 11:25:15 -0300

    Primer Commit
    Estos son los 4 primeros ejercicios de la primera clase/Primer Commit

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	/*El promot sirve como entrada y salida 
+	El promot sirve como entrada y salida
+	El promot sirve como entrada y salida*/
+	var nombre; /* Esta funcion siempre */
+	nombre = prompt("Ingrese su nombre");
+	alert("Su nombre es:"+nombre);
+
+}
+
+/*2 Alert : ("Nombre");
+(Nombre); (Sin comillas es para variable especifica)*/
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,13 @@
+/* 
+	Debemos lograr tomar un dato por 'prompt' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var nombre;	
+	nombre = prompt("Nombre del dato","Hola");
+	document.getElementById('elNombre').value="Hola"
+}
+
+/*2 TIPOS DE VENTANA : alert MUESTRA, "prompt" TOMA
+document.getElemntById('nombre').value="Hola"*/
