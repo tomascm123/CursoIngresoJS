e432d61ffc7f5c74d9617e8b8265f9f48c78ff2d
tomascm123 <40763524+tomascm123@users.noreply.github.com>
2018-07-07 20:00:56 -0300

    commit
    -10

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -2,13 +2,22 @@
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
-function MostrarAumento()
+function mostrarAumento()
 {
-	var importe;
-	var resultado;
-	importe=document.getElementById('importe').value;
-	importe=parseInt(importe);
-	resultado=importe-(importe*0.25);
-		//	resultado=importe*0.4 esta mal hecho
-	document.getElementById('resultado').value=resultado;
+	
+var	importe;
+var resultado;
+var descuento;
+
+importe = document.getElementById('importe').value;
+importe = parseInt (importe);
+resultado = parseInt (resultado);
+descuento = 0.25 ;
+resultado = document.getElementById('resultado').value=(importe * descuento);
+resultado = importe - resultado ;
+alert("EL resultado con descuento es " +resultado);
+
+
+
+
 }
